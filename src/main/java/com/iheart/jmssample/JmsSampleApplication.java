0525847705d1056db70b0167cb37d72c623f17f0
Fernando Matias Valadao <fvaladao@ciandt.com>
2018-09-10 16:07:40 -0300

    Added two values in application.properties to refer to RabbitMQ host and queue name

--- src/main/java/com/iheart/jmssample/JmsSampleApplication.java
+++ src/main/java/com/iheart/jmssample/JmsSampleApplication.java
@@ -1,23 +1,22 @@
 package com.iheart.jmssample;
 
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.AsyncRabbitTemplate;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.core.Binding;
-
 
 @SpringBootApplication
 public class JmsSampleApplication {
-	private static final String queueName = "my-test-queue";
+    @Value("${rabbitmq.host}")
+	private String rabbitMqHost;
+
+    @Value("${rabbitmq.queue}")
+	private String queueName;
 
 	@Bean
 	Queue queue() {
@@ -26,9 +25,7 @@ public class JmsSampleApplication {
 
 	@Bean
 	ConnectionFactory connectionFactory() {
-		CachingConnectionFactory connectionFactory = new CachingConnectionFactory("172.16.106.194");
-		connectionFactory.setUsername("test");
-		connectionFactory.setPassword("test");
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory(rabbitMqHost);
 
 		return connectionFactory;
 	}
